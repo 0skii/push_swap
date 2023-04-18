f829afe15701343ceeeea40a87801e57b36f336b
Oscar Amado de Carvalho <o.amado@orange.fr>
2023-04-18 21:30:58 +0100

    Good without cost on b

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ozerbib- <ozerbib-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 11:26:21 by ozerbib-          #+#    #+#             */
-/*   Updated: 2023/04/18 11:49:24 by ozerbib-         ###   ########.fr       */
+/*   Updated: 2023/04/18 17:16:01 by ozerbib-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void		node_move(t_head *list, t_list *node, int bora);
 int			freaknorm(t_head *list);
 t_list		*phone_a_friend(t_head *stack_a, t_list *b);
 void		go_to_friend(t_head *stack_a, t_head *stack_b);
+void		min_to_top(t_head *stack_a);
 /*Algos*/
 void		mini_sort3_pt1(t_head *list);
 void		mini_sort3_pt2(t_head *list);
